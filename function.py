7d93466c0c0bf9ce8ae182ace459edb7d7e8bf4a
Tom Wu <qwu@microsoft.com>
2024-05-02 10:17:25 -0400

    init

---
+++ function.py
@@ -0,0 +1,13 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+@app.function_name(name="HttpTrigger1")
+@app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    return func.HttpResponse(
+        "This HTTP triggered function executed successfully.",
+        status_code=200
+        )
